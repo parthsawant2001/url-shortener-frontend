550821a8bbd08f5f49f1e6e4f7aba8f2cdaade33
parthsawant2001 <parthpsawant2001@gmail.com>
2023-06-04 13:54:06 +0530

    first main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
         },
         {
           headers: {
-            'content-type': 'application/json',
+            'content-type': 'application/x-www-form-urlencoded',
           },
         }
       )
