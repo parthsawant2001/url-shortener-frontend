13fe8062b3686255035e3ea96463cbb5d35c55e3
parthsawant2001 <parthpsawant2001@gmail.com>
2023-06-04 13:03:57 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image';
+// import React, { useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+export default function Home() {
+  const [url, setUrl] = useState();
+  const [shortUrl, setShortUrl] = useState('');
+
+  const onSubmit = () => {
+    console.log('urlll:', url);
+    axios
+      .post(
+        'http://localhost:5000/shorten',
+        {
+          url: url,
+        },
+        {
+          headers: {
+            'content-type': 'application/json',
+          },
+        }
+      )
+      .then((res) => {
+        setShortUrl(`http://localhost:5000/${res.data.shortCode}`);
+        console.log('frontend axios call', res.data.shortCode);
+      })
+      .catch((err) => console.error('error', err));
+  };
+
+  return (
+    <div className='grid h-screen place-items-center'>
+      <div className='border p-20 rounded-md'>
+        <h1>URL Shortener</h1>
+        <input
+          className='p-[10px] my-[10px] text-black rounded-md'
+          type='text'
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder='Enter URL'
+        />
+        <button
+          onClick={onSubmit}
+          className='bg-[#ffc0c0] text-black p-[10px] m-[10px] rounded-md'
+        >
+          Shorten
+        </button>
+        <br />
+        <a>{shortUrl}</a>
+      </div>
+    </div>
+  );
+}
